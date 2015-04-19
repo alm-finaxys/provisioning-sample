99c313c7ab64eee0d2c899ecab2952c890e14098
alm-finaxys <sedat.guclu@finaxys.com>
2015-04-19 23:51:40 +0200

    Update puppet.pp
    hosts updated

--- puppet.pp
+++ puppet.pp
@@ -12,66 +12,16 @@ user { 'vagrant':
   password => '$1$eIqlEIzY$zPj3zDvToC8rMnPbdKZv70',
 }
  
+host { 'cinode':
+    ip => '192.168.1.12',
+}
+ 
 File { owner => 0, group => 0, mode => 0644 }
 file { '/etc/motd':
 content => "CENTOS 64 BITS GENERATED NODE (Puppet over Vagrant)\n"
 }
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-
-#class system-update {
-#  exec { 'apt-get update':
-#    command => 'apt-get update',
-#  }
-
-#  $sysPackages = [ "build-essential" ]
-#  package { $sysPackages:
-#    ensure => "installed",
-#    require => Exec['apt-get update'],
-#  }
-#}
-
-# class tomcat7 {
- 
-#  package { "tomcat7":
-#    ensure => present,
-#    require => Class["system-update"],
-#  }
- 
-#  service { "tomcat7":
-#    ensure => "running",
-#    require => Package["tomcat7"],
-#  }
-# 
-# }
- 
-# include tomcat7
-# include system-update
-
-# S. GUCLU : tweak used to generate missing links on Tomcat 7 for runtime
-# file { '/var/lib/tomcat7/bin':
-#   ensure => 'link',
-#   require => Package["tomcat7"],
-#   target => '/usr/share/tomcat7/bin',
-#}
-#file { '/var/lib/tomcat7/lib':
-#   ensure => 'link',
-#   require => Package["tomcat7"],
-#   target => '/usr/share/tomcat7/lib',
-#}
-
-# S. GUCLU : tweak used to disable authentication issues on sudo-managed deployments
-#file { '/tmp/sudo-update.sh':
-#   ensure => 'present',
-#   mode => '+x',
-#   require => Package["tomcat7"],
-#   content => 'echo "vagrant ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/vagrant',
-#}
-
-#exec { 'sudo all for vagrant user':
-#   command => 'sudo /tmp/sudo-update.sh',
-#   require => File["/tmp/sudo-update.sh"],
-#}
 
 node /centos64autoslave.*/ {
   class { 'jenkins::slave':
