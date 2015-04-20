48472e6f33b67ddafff8fbd9bd0c98e588c35520
alm-finaxys <sedat.guclu@finaxys.com>
2015-04-21 00:34:41 +0200

    Update puppet.pp

--- puppet.pp
+++ puppet.pp
@@ -2,20 +2,33 @@
 # Author: S. Guclu
 # description: main driver script for Puppet
 # read more puppet syntax @ http://www.puppetcookbook.com/
+
 #make sure the puppet is installed on the .box otherwise rest of the puppet provisioning will not function
 group { "puppet":
 ensure => "present",
 }
 
+# vagrant account password reset to connect
 user { 'vagrant': 
   ensure   => present,
   password => '$1$eIqlEIzY$zPj3zDvToC8rMnPbdKZv70',
 }
  
+# /etc/host setup with private address  
 host { 'cinode':
     ip => '192.168.1.12',
 }
- 
+
+# java truststore update with root CA
+java_ks { 'puppetca:truststore':
+  ensure       => latest,
+  certificate  => '/tmp/cacerts',
+  target       => '/usr/lib/jvm/java/jre/lib/security/cacerts',
+  password     => 'changeit',
+  trustcacerts => true,
+}
+
+# various options 
 File { owner => 0, group => 0, mode => 0644 }
 file { '/etc/motd':
 content => "CENTOS 64 BITS GENERATED NODE (Puppet over Vagrant)\n"
@@ -23,6 +36,7 @@ content => "CENTOS 64 BITS GENERATED NODE (Puppet over Vagrant)\n"
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+# slave registration through swarm plugin
 node /centos64autoslave.*/ {
   class { 'jenkins::slave':
     masterurl => 'https://cinode:44312',
